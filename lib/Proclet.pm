d2bdbde6cb2c41467908fdf009e4a60222315d62
Masahiro Nagano <kazeburo@gmail.com>
2012-06-14 00:40:49 +0900

    initial commit

---
+++ lib/Proclet.pm
@@ -0,0 +1,32 @@
+package Proclet;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Proclet -
+
+=head1 SYNOPSIS
+
+  use Proclet;
+
+=head1 DESCRIPTION
+
+Proclet is
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
