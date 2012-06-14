e008a4b725dc34eefca0018d80e8ed8ac3adec21
Masahiro Nagano <kazeburo@gmail.com>
2012-06-14 17:09:32 +0900

    add test_requires

--- lib/Proclet.pm
+++ lib/Proclet.pm
@@ -156,7 +156,7 @@ Proclet - minimalistic Supervisor
 
 =head1 DESCRIPTION
 
-Proclet is minimalistic Supervisor, fork and manage services from one perl script.
+Proclet is minimalistic Supervisor, fork and manage many services from one perl script.
 
 =head1 METHOD
 
